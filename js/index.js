e154bea711cc6752c5a934677f5cd9815edd8272
teha007 <teha007@naver.com>
2019-12-26 15:59:25 +0900

    C 언어 설명 페이지 작성 완료

--- js/index.js
+++ js/index.js
@@ -1,4 +1,16 @@
-$function loaded() {
-    $("#include").load("../index2.html");
+function loaded() {
+    var e=document.getElementById("map");
+    var location=new google.maps.LatLng(37.562161,127.035199);
+    var option={
+        center:location,
+        zoom:17
+    };
+    var marker=new google.maps.Marker({position:location,title:"미래능력개발교육원"});
+
+    var map=new google.maps.Map(e,option);
+    marker.setMap(map);
+    marker.addListener('click',function() {
+        open('http://www.mrhi.or.kr','_blank');
+    })
 }
 
